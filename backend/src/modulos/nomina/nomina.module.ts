faeeed286d2e4e0c4f418dc7bec44348c3f2d763
Federico Roldan <federoldan.fr@gmail.com>
2026-02-01 18:59:45 -0300

    Actualiza back

--- backend/src/modulos/nomina/nomina.module.ts
+++ backend/src/modulos/nomina/nomina.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { NominaController } from './nomina.controller';
 import { NominaService } from './nomina.service';
 import { AuditService } from '../../common/audit.service';
+import { PrismaService } from '../../common/prisma.service';
 
 @Module({
   controllers: [NominaController],
-  providers: [NominaService, AuditService],
+  providers: [NominaService, PrismaService, AuditService],
 })
 export class NominaModule {}
--- backend/src/modulos/terceros/terceros.module.ts
+++ backend/src/modulos/terceros/terceros.module.ts
@@ -3,10 +3,11 @@ import { TercerosService } from './terceros.service';
 import { TercerosController } from './terceros.controller';
 import { TercerosImportController } from './terceros.import.controller';
 import { AuditService } from '../../common/audit.service';
+import { PrismaService } from '../../common/prisma.service';
 
 @Module({
   controllers: [TercerosController, TercerosImportController],
-  providers: [TercerosService, AuditService],
+  providers: [TercerosService, PrismaService, AuditService],
   exports: [TercerosService],
 })
 export class TercerosModule {}
